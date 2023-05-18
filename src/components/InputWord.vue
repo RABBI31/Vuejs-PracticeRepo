708466407ee972dc0987319cb65afba970793c81
Softzino Dev <18103380@iubat.edu>
2023-05-18 19:21:48 +0600

    Hangman with hints

---
+++ src/components/InputWord.vue
@@ -0,0 +1,22 @@
+<template>
+  <div style="margin-top: 100px;">
+      <p>Hints for the letter</p>
+      {{ hints }}
+    </div>
+    <div class="word">
+      <span class="letter" v-for="(letter=0, i) in letters" :key="i">
+        {{ correctLetters.includes(letter) ? letter : '' }}
+      </span>
+    </div>
+    
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      hints:{ type: String, required: true},
+      letters: { type: Array, required: true },
+      correctLetters: { type: Array, default: () => [] }
+    }
+  }
+  </script>
--- src/components/MyApp.vue
+++ src/components/MyApp.vue
@@ -1,9 +1,106 @@
-<script setup>
-import '../assets/style.css'
-import Header from './AppHeader.vue';
-
-</script>
-
 <template>
-   <Header />
-</template>
+   <Header  />
+   <div class="game-container">
+     <Figure :wrong-count="wrongLetters.length" />
+     <WrongLetters :wrong-letters="wrongLetters" />
+     <Word :hints="hints" :letters="letters" :correct-letters="correctLetters" />
+   </div>
+   <AppPopup :status="status" :word="word" @reset="reset" />
+   <Notification :show="notification" />
+ </template>
+ 
+ <script>
+ import { computed, ref } from 'vue'
+ 
+ import '../assets/style.css'
+ 
+ import Header from './AppHeader.vue'
+ import Figure from './AppFigure'
+ import WrongLetters from './WrongLetter'
+ import Word from './InputWord'
+ import AppPopup from './AppPopup'  
+ import Notification from './AppNotification'
+ 
+ import onKeydown from '../assets/onKeyDown'
+
+const words =[
+  {
+       tword:'Red',
+       hints:"One of the Three main color"
+
+ },
+ {
+       tword:'Javascript',
+       hints:"One of the demanding Programming language"
+
+ },
+ {
+      tword:'Mango',
+       hints:"One of the favourite fruits for all"
+
+ }
+
+]
+const randomIndex = Math.floor(Math.random() * words.length);
+ const randomWord =()=> words[randomIndex].tword;
+ const randomHints =()=> words[randomIndex].hints;
+
+ 
+ export default {
+   components: { Header, Figure, Word, WrongLetters, AppPopup, Notification },
+   setup() {
+     const word = ref(randomWord())
+     const hints = ref(randomHints())
+    
+     const guessedLetters = ref([])
+ 
+     const letters = computed(() => word.value.split(''))
+    const wrongLetters = computed(() =>
+      guessedLetters.value.filter(l => !letters.value.includes(l))
+    )
+    const correctLetters = computed(() =>
+      guessedLetters.value.filter(l => letters.value.includes(l))
+    )
+
+    const status = computed(() => {
+      if (wrongLetters.value.length === 6) return 'lose'
+      if (letters.value.every(l => correctLetters.value.includes(l)))
+        return 'win'
+      return ''
+    })
+    const reset = () => {
+      guessedLetters.value = []
+      word.value = randomWord()
+    }
+
+    const notification = ref(false)
+    const showNotification = () => {
+      notification.value = true
+      setTimeout(() => (notification.value = false), 2000)
+    }
+
+    onKeydown(event => {
+      const letter = event.key.toLowerCase()
+      if (event.keyCode < 65 || event.keyCode > 90) return
+      if (status.value) return
+      if (guessedLetters.value.includes(letter)) {
+        showNotification()
+        return
+      }
+      guessedLetters.value.push(letter)
+    })
+
+    return {
+      letters,
+      word,
+      wrongLetters,
+      correctLetters,
+      guessedLetters,
+      notification,
+      status,
+      hints,
+      reset
+    }
+  }
+}
+</script>
